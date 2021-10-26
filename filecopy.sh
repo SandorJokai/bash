c84b59f91922bdb324c05154d66c0ed451957f88
SandorJokai <60708018+SandorJokai@users.noreply.github.com>
2021-10-26 16:32:30 +0100

    Add files via upload

---
+++ filecopy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source="$1"
+destination="$2"
+
+if [ $# != 2 ]
+then
+#  echo "You need to type the source and the destination IP adresses as arguments!"
+  echo;echo "You need to type both the <src> IP's and the <dest> IP's!";echo
+  exit 1
+fi
+
+array=(/root/filecopy /root/testfile)
+
+for i in "${array[@]}"
+do
+#  `ssh root@$source "tar -cf compressed.tar $i"`
+  ssh root@$source "tar -cf - $i" | ssh root@$destination "tar -xf - -C /"
+done
